b1665907513936b01deaaef3ba50e75bbef46405
F1N3X <quartzman92@gmail.com>
2025-10-17 15:54:04 +0200

    premier test de total de panier qui échoue

--- index.test.ts
+++ index.test.ts
@@ -1,1 +1,11 @@
 import { describe, test, expect } from "@jest/globals"
+
+describe('Calculer le total du panier', () =>{
+    const panier = [
+        { nom: "pomme", prix: 1 },
+        { nom: "banane", prix: 1 },
+    ];
+    test('Calculer le total du panier', () =>{
+        expect(0).toEqual(2)
+    })
+})
